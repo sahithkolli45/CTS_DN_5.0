946bacffea0266afe61c59aa0d30b220f7d18c89
Sahith Kolli <sahithkolli45@gmail.com>
2026-06-23 11:08:21 +0530

    Added Singleton Pattern Example

---
+++ SingletonPatternExample/Logger.java
@@ -0,0 +1,18 @@
+package SingletonPatternExample;
+
+public class Logger {
+    private static Logger instance;
+
+    private Logger(){
+        System.out.println("Logger instance created.");
+    }
+    public static Logger getInstance(){
+        if (instance == null){
+            instance = new Logger();
+        }
+        return instance;
+    }
+    public void Log(String message){
+        System.out.println("log: " + message);
+    }
+}
---
+++ SingletonPatternExample/SingletonTest.java
@@ -0,0 +1,18 @@
+package SingletonPatternExample;
+
+public class SingletonTest {
+    public static void main(String[] args){
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
+
+        logger1.Log("First Message");
+        logger2.Log("Second Message");
+
+        if(logger1 == logger2){
+            System.out.println("Only one Logger instance available");
+        }else{
+            System.out.println("Multiple Logger instance available");
+        }
+    }
+    
+}
